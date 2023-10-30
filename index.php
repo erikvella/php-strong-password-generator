b6c367287d711324eae59ddbd926d9f0f3d19973
erikvella <vella.erik1@gmail.com>
2023-10-30 16:43:39 +0100

    [fix] password generator

--- index.php
+++ index.php
@@ -4,10 +4,21 @@
 
 $characters = ['q' , 'Q' , 'e' , 'E' , 'r' , 'R' , 't' , 'T' , 'y' , 'Y' , 'u' , 'U' , 'i' , 'I' , 'o' , 'O' , 'p' , 'P' , 'a' , 'A' , 's' , 'S' , 'd' , 'D' , 'f' , 'F' , 'g' , 'G' , 'h' , 'H' , 'j' , 'J' , 'k' , 'K' , 'l' , 'L' , 'z' , 'Z' , 'x' , 'X' , 'c' , 'C' , 'v' , 'V' , 'b' , 'B' , 'n' , 'N' , 'm' , 'M' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '0' , '!' , '?' , '(' , ')' , '[' , ']' , '{' , '}' , '#' , '+' , '-' , '/' , '*' , '$' , '&' , '£' , '%' , '=' , '<' , '>'];
 
+$password = '';
 
 include __DIR__ . "/partials/head.php";
-$limit = '';
+$limit = $_POST['quantity'];
 
+
+
+if(isset($_POST['quantity']) && !empty($_POST['quantity'])){
+  // controllo se il valore inserito è corretto anche se ho già messo il controllo su html , mettiamo il controllo anche lato server per sicurezza
+  for($i = 0 ; $i < $limit ; $i++){
+    $getRandomIndex =  rand(0 , 79);
+    $char = $characters[$getRandomIndex];
+    $password .= $char;
+  }
+}
 
 
 
@@ -29,6 +40,10 @@ $limit = '';
       <div class="col-4">
         <h2 class="my-5">Scegli il numero di caratteri che dovrà avere la tua password con un massimo di 32 e un minimo di 8</h2>
 
+
+                                            <!-- inizio del form -->
+
+
         <form action="index.php" method="POST">
   <div class="mb-3">
     <label for="quantity" class="form-label">Numero di caratteri (8/32)</label>
@@ -37,14 +52,15 @@ $limit = '';
   </div>
  
   <button type="submit" class="btn btn-primary">Genera password</button>
-  <button type="submit" class="btn btn-danger">Annulla</button>
+  <button type="reset" class="btn btn-danger">Annulla</button>
 </form>
         
       </div>
 
 
       <div class="col-4 my-5">
-        <h2>Password generata : </h2>
+        <h2>Password generata : <br>
+        <?php echo $password ?></h2>
       </div>
       
     </div>
